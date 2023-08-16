f68fd1c53f860bc18340b32390b1f7e6cf5e5612
GeorgeMedhat <92307957+GeorgeMedhat@users.noreply.github.com>
2023-08-16 20:48:33 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,15 +22,68 @@ class HomeScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:Color.fromRGBO(203, 195, 227,1) ,
-      body: SafeArea(
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset("Assets/images/task2_image.png"),
+     body: Center(
 
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(40),
+
+          ),
+          width: MediaQuery.of(context).size.width -MediaQuery.of(context).size.width/15,
+          height:MediaQuery.of(context).size.height -MediaQuery.of(context).size.height/15,
+          child: Column (
+
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height/20),
+             Container(
+               width:MediaQuery.of(context).size.width/1.3 ,
+               height: MediaQuery.of(context).size.height/2.2,
+               decoration: BoxDecoration(
+                 image: DecorationImage(
+                   image: AssetImage("Assets/images/task2_image.png"),
+                   fit: BoxFit.fill
+                 )
+               ),
+             ),
+              SizedBox(height: MediaQuery.of(context).size.height/12,),
+              Text("Safe Payment ,Happy You!",style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25
+              ),),
+              SizedBox(height: MediaQuery.of(context).size.height/40,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: MediaQuery.of(context).size.width/12,),
+                  Expanded(child: Text("Create a fresh virtual credit card for all your "
+                      "shoppings and banking needs.",style: TextStyle(
+                    wordSpacing: 3,
+                      overflow: TextOverflow.clip,
+                      fontSize: 15
+                  ),))
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height/10,),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                    color: Color.fromRGBO(138,43,226,1)
+                  ),
+                  width: MediaQuery.of(context).size.width/5,
+
+                  child:
+                      Center(
+                         child: Icon(Icons.arrow_forward_rounded,size: 50,color: Colors.white,)
+                      ),
+                  ),),
+
+            ],
+          ),
         ),
-      ),
+     ),
     );
   }
   
